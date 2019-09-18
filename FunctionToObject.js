f84a0789abd659b6828b4f2e50d45eb6246a8094
Glahatte <55100132+Glahatte@users.noreply.github.com>
2019-09-17 20:15:51 -0400

    Add files via upload
    I dont understand this. I "get" it but I dont.

---
+++ FunctionToObject.js
@@ -0,0 +1,10 @@
+
+function fahToCell(fahrenheit) 
+{
+    let fahrenheit=20;
+  var fTemp = fahrenheit;
+  var fToCel = (fTemp - 32) * 5 / 9;
+} 
+fahToCell=function(){
+    console.log ("Its gonna be great out!")
+};
